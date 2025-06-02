daa782cc04850d165e9193f3e3b125a2c2a0823f
Andrés Bórquez <andres.borquezc@gmail.com>
2025-06-03 00:34:04 +0200

    renamed KFParticle.h -> KFParticle.hxx

--- KFParticle/KFParticle.cxx
+++ KFParticle/KFParticle.cxx
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "KFParticle.h"
+#include "KFParticle.hxx"
 #include "KFParticle_Math.hxx"
 
 #include <algorithm>
